882030a1b8890bcfb6327749a15b2a01f25a9bd7
cpan1995 <cpan1995@gmail.com>
2021-06-28 12:39:18 -0400

    commit tree bb996582f1d2bb3aa612e6ccfb7e87b8abf9f38c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(someString = 'roller-skate'){
+    return `This Saturday, I want to ${someString}!`;
+}
+function mondayWork(someString = 'go to the office'){
+    return `This Monday, I will ${someString}.`;
+}
+function wrapAdjective(wrapper='*'){
+    return function (someString){
+        return `You are ${wrapper}${someString}${wrapper}!`; 
+    }
+}
+
